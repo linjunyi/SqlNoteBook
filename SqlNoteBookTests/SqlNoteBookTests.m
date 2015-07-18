18e6108831fa68e8bbd6b8fe0306f3630c4ddf84
林君毅 <522526435@qq.com>
2015-07-18 18:21:29 +0800

    Initial Commit

---
+++ SqlNoteBookTests/SqlNoteBookTests.m
@@ -0,0 +1,40 @@
+//
+//  SqlNoteBookTests.m
+//  SqlNoteBookTests
+//
+//  Created by lin on 15/7/18.
+//  Copyright (c) 2015年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface SqlNoteBookTests : XCTestCase
+
+@end
+
+@implementation SqlNoteBookTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
